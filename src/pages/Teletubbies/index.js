f39b939f0f58a379399809325f9a7c28b0d376bf
carisno <carisnoremes@gmail.com>
2023-06-08 09:31:56 +0900

    add search function by name to index.js

--- src/pages/Teletubbies/index.js
+++ src/pages/Teletubbies/index.js
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from "react";
 import {
   Container,
   Typography,
-  Box,
   Grid,
   Button,
   Avatar,
+  TextField,
+  InputAdornment,
 } from "@mui/material";
 import teletubbiesData from "../../Data/teletubbies.json";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Tele = () => {
   const [teletubbies, setTeletubbies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     // Load the first 20 Teletubbies on mount
@@ -18,8 +21,11 @@ const Tele = () => {
   }, []);
 
   function handleScroll() {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-      // Load the next 20 Teletubbies
+    if (
+      window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+      searchTerm === ""
+    ) {
+      // Load the next 20 Teletubbies if search term is empty
       const startIndex = teletubbies.length;
       const endIndex = startIndex + 20;
       setTeletubbies([
@@ -37,6 +43,11 @@ const Tele = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   });
+
+  const filteredTeletubbies = teletubbies.filter((teletubby) =>
+    teletubby.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
       <Container>
@@ -53,7 +64,40 @@ const Tele = () => {
             Teletubbies
           </Typography>
         </Grid>
-        {teletubbies.map((teletubby, index) => (
+        <TextField
+          fullWidth
+          variant="outlined"
+          placeholder="Search Teletubbies by name"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+            sx: {
+              bgcolor: "background.paper",
+              borderRadius: 2,
+              "& .MuiOutlinedInput-notchedOutline": {
+                borderColor: "primary.main",
+              },
+              "&:hover .MuiOutlinedInput-notchedOutline": {
+                borderColor: "primary.light",
+              },
+              "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                borderColor: "secondary.main",
+              },
+              "& .MuiOutlinedInput-input": {
+                color: "text.primary",
+              },
+              "& .MuiInputLabel-root": {
+                color: "text.secondary",
+              },
+            },
+          }}
+        />
+        {filteredTeletubbies.map((teletubby, index) => (
           <Grid
             container
             sx={{
@@ -99,16 +143,16 @@ const Tele = () => {
                       sx={{ my: 2 }}
                     >
                       <Button variant="outlined" color="primary" sx={{ mx: 2 }}>
-                        traits1
+                        {teletubby.traits[0]}
                       </Button>
                       <Button variant="outlined" color="primary" sx={{ my: 2 }}>
-                        traits2
+                        {teletubby.traits[1]}
                       </Button>
                       <Button variant="outlined" color="primary" sx={{ mx: 2 }}>
-                        traits3
+                        {teletubby.traits[2]}
                       </Button>
                       <Button variant="outlined" color="primary">
-                        traits4
+                        {teletubby.traits[3]}
                       </Button>
                     </div>
                   </Grid>
@@ -116,7 +160,7 @@ const Tele = () => {
               </>
             ) : (
               <>
-                 <Grid item xs={12} md={6} lg={10}>
+                <Grid item xs={12} md={6} lg={10}>
                   <Grid component="div">
                     <Typography
                       variant="h4"
